1b21bb7de7db1110cc7f6ddf9cf9aabdd3a19002
Maxime OUAIRY <maxime.ouairy-ext@consensys.net>
2026-02-26 17:53:35 +0100

    feat: tx-ctrl add forceGasFeeToken param

--- packages/transaction-controller/src/types.ts
+++ packages/transaction-controller/src/types.ts
@@ -1777,6 +1777,9 @@ export type TransactionBatchSingleRequest = {
 export type TransactionBatchRequest = {
   batchId?: Hex;
 
+  /** Added for tx to appear in Activity tab */
+  actionId?: string;
+
   /** Whether to disable batch transaction processing via an EIP-7702 upgraded account. */
   disable7702?: boolean;
 
@@ -1794,6 +1797,10 @@ export type TransactionBatchRequest = {
 
   /** Address of an ERC-20 token to pay for the gas fee, if the user has insufficient native balance. */
   gasFeeToken?: Hex;
+
+  /** When set to `true` and if gasFeeToken is set, use gasFeeToken regardless of user native balance. */
+  /** Unless true, gasFeeToken is only taken as a suggestion and native balance will be used in batch 7702 transactions */
+  forceGasFeeToken?: boolean;
 
   /** Gas limit for the transaction batch if submitted via EIP-7702. */
   gasLimit7702?: Hex;
@@ -2165,6 +2172,10 @@ export type AddTransactionOptions = {
 
   /** Whether MetaMask will sponsor the gas fee for the transaction. */
   isGasFeeSponsored?: boolean;
+
+  /** When set to `true` and if gasFeeToken is set, use gasFeeToken regardless of user native balance. */
+  /** Unless true, gasFeeToken is only taken as a suggestion and native balance will be used in batch 7702 transactions */
+  forceGasFeeToken?: boolean;
 
   /**
    * Whether the transaction has no lifecycle and is not signed or published.
